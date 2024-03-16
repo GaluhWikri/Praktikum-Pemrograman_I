86210409cf8302f1cffeeb47f8eef7a2d097e2db
GaluhWikri <115914762+GaluhWikri@users.noreply.github.com>
2024-03-17 02:33:17 +0700

    Add files via upload

---
+++ node.java
@@ -0,0 +1,26 @@
+package Latihan6;
+
+public class node {
+			 private int data;
+			    private node next;
+
+			    public node(int data) {
+			        this.data = data;
+			    }
+
+			    // Setter dan Getter
+			    public void setNext(node next) {
+			        this.next = next;
+			    }
+
+			    public int getData() {
+			        return data;
+			    }
+
+			    public node getNext() {
+			        return next;
+			    }
+		}
+
+
+
